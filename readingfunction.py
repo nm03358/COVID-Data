49849c2c29f88a67f6e867de22f3483a53fc82bf
Peter Mourfield <1678724+peter-mourfield@users.noreply.github.com>
2021-03-02 16:48:07 -0500

    Update readingfunction.py

--- readingfunction.py
+++ readingfunction.py
@@ -3,7 +3,7 @@
 
 import pandas as pd
 
-#read = pd.read_csv(r'https://github.com/nm03358/COVID-Data/blob/National/all-states-history.csv', index_col=0)
+#read = pd.read_csv(r'https://raw.githubusercontent.com/nm03358/COVID-Data/National/all-states-history.csv', index_col=0)
 read = pd.read_csv(r'file://localhost/Users/nich/Desktop/Capstone/Code/data/helpmeplz.csv', index_col=0)
 df = read
 print(df.head(5))
