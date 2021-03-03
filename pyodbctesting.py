f857a7e4958bc1f8555ae52e88903d64be54a039
nm03358 <77293889+nm03358@users.noreply.github.com>
2021-03-03 11:30:02 -0500

    Update pyodbctesting.py

--- pyodbctesting.py
+++ pyodbctesting.py
@@ -1,5 +1,8 @@
+#Nicholas Mourfield
+
 import pyodbc
 import pandas as pd
+import sqlalchemy
 
 databaseName = 'covid-project-db'
 username = 'NMourfield'
@@ -8,12 +11,21 @@ server = 'covid-project-server.database.windows.net'
 driver= '{ODBC Driver 17 for SQL Server}'
 CONNECTION_STRING = 'DRIVER='+driver+';SERVER='+server+';DATABASE='+databaseName+';UID='+username+';PWD='+password
 
-sql="SELECT * FROM dbo.alabama_history"
-
 conn = pyodbc.connect(CONNECTION_STRING)
 cursor = conn.cursor()
-cursor.execute(sql)
-data = cursor.fetchall()
 
-df = pd.DataFrame(data)
-print(df['date'])
+statenames = open(r"c:/Users/nm03358\Documents/Capstone/txtfiles/statenames.txt")
+lines = statenames.readlines()
+count = 0
+urllist = []
+for i in lines:
+    i = i.rstrip()
+    url = ("https://raw.githubusercontent.com/nm03358/COVID-Data/States/"+i+"-history.csv")
+    urllist.append(url)
+
+for url in urllist:
+    read = pd.read_csv(url, index_col=0)
+    df = read
+    print(df)
+
+conn.close()
