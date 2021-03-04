7ca9e34d9746c1b0b4e697ba49daa8dde485d57d
nm03358 <77293889+nm03358@users.noreply.github.com>
2021-03-04 15:11:38 -0500

    Update pyodbctesting.py

--- pyodbctesting.py
+++ pyodbctesting.py
@@ -3,6 +3,8 @@
 import pyodbc
 import pandas as pd
 import sqlalchemy
+from sqlalchemy import Table, MetaData, Column, Integer
+import urllib
 
 databaseName = 'covid-project-db'
 username = 'NMourfield'
@@ -14,18 +16,25 @@ CONNECTION_STRING = 'DRIVER='+driver+';SERVER='+server+';DATABASE='+databaseName
 conn = pyodbc.connect(CONNECTION_STRING)
 cursor = conn.cursor()
 
-statenames = open(r"c:/Users/nm03358\Documents/Capstone/txtfiles/statenames.txt")
-lines = statenames.readlines()
+params = urllib.parse.quote_plus("DRIVER={SQL Server Native Client 10.0};SERVER=covid-project-server.database.windows.net;DATABASE=covid-project-db;UID=NMourfield;PWD=Pa$$word")
+engine = sqlalchemy.create_engine("mssql+pyodbc:///?odbc_connect=%s" % params)
+
+statenames = ['alabama', 'alaska', 'arizona', 'arkansas', 'california', 'colorado', 'connecticut', 'delaware', 'florida', 'georgia', 'hawaii', 'idaho', 'illinois', 'indiana', 'iowa', 'kansas', 'kentucky', 'louisiana', 'maine', 'maryland', 'massachusetts', 'michigan', 'minnesota', 'mississippi', 'missouri', 'montana', 'nebraska', 'nevada', 'new-hampshire', 'new-jersey', 'new-mexico', 'new-york', 'north-carolina', 'north-dakota', 'ohio', 'oklahoma', 'oregon', 'pennsylvania', 'rhode-island', 'south-carolina', 'south-dakota', 'tennessee', 'texas', 'utah', 'vermont', 'virginia', 'washington', 'west-virginia', 'wisconsin', 'wyoming']
 count = 0
 urllist = []
-for i in lines:
+tablenames = []
+for i in statenames:
     i = i.rstrip()
     url = ("https://raw.githubusercontent.com/nm03358/COVID-Data/States/"+i+"-history.csv")
     urllist.append(url)
+    tablenames.append(i)
 
-for url in urllist:
-    read = pd.read_csv(url, index_col=0)
-    df = read
-    print(df)
+datapass = {tablenames[i]: urllist[i] for i in range(len(tablenames))} 
+
+for i in datapass:
+    df = pd.read_csv(datapass[i], index_col=0)
+    print(i)
+    print(df.head(5))
+    #df.to_sql(i, engine)
 
 conn.close()
